44e1bb032f31739e558db8d1313db6e073da6aa7
Rodrigo Alves Fontanella <fontanellarodrigo13@gmail.com>
2022-05-05 15:55:47 -0300

    about view

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,5 @@
 package application;
-
-
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,25 +8,30 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	private static Scene mainScene;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-
+			
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 
-			Scene mainScene = new Scene(scrollPane);
+			
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
-		
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public static Scene getMainScene() {
+		return mainScene;
+	}
 
 	public static void main(String[] args) {
 		launch(args);
--- src/module-info.java
+++
@@ -1,6 +0,0 @@
-module javafx_jdbc {
-	requires javafx.controls;
-	requires javafx.fxml;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
