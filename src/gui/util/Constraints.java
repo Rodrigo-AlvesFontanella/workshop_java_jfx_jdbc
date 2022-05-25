47ea1ac68d1a7d91714020e75833a667d44f1630
Rodrigo Alves Fontanella <fontanellarodrigo13@gmail.com>
2022-05-25 14:27:50 -0300

    TextField & DatePicker

--- src/gui/util/Constraints.java
+++ src/gui/util/Constraints.java
@@ -27,4 +27,12 @@ public class Constraints {
                 }
 		    });
 	}
+	
+	public static void setTextFieldName(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && !newValue.matches("\\sa-zA-Z*\\ã\\á\\é")) {
+				txt.setText(newValue.replaceAll("[^\\sa-zA-Z\\ã\\á\\é]", ""));
+			}
+		});
+	}
 } 
